145514e0cb1c9b333c26086d1ee9aaed3283fa0b
The Fixer <info@lanonasis.com>
2026-01-09 14:23:49 +0100

    feat: add project configuration files for oauth-client and security-sdk, remove vitest workspace
    Signed-off-by: The Fixer <info@lanonasis.com>

--- vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './web/vitest.config.ts',
-  './oauth-client/vitest.config.ts',
-  './security-sdk/vitest.config.ts',
-]);
